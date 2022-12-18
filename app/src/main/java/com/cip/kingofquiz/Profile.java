c64a8d368f75749982b26fd2202fe663a85cfa6e
a.rahimzadeh <a.rahimzadeh@emofid.com>
2022-12-18 14:05:30 +0330

    Set profiles fields data.

--- app/src/main/java/com/cip/kingofquiz/Profile.java
+++ app/src/main/java/com/cip/kingofquiz/Profile.java
@@ -3,10 +3,13 @@ package com.cip.kingofquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.cip.kingofquiz.db.AppDatabase;
 import com.cip.kingofquiz.model.User;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Profile extends AppCompatActivity {
 
@@ -18,8 +21,20 @@ public class Profile extends AppCompatActivity {
         AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
         User user = db.userDao().getLoggedInUser();
 
-        TextView name = (TextView) findViewById(R.id.usernameProfile);
+        TextView score = (TextView) findViewById(R.id.profileScore);
+        TextView name = (TextView) findViewById(R.id.profileUsername);
+        EditText emailField = (EditText) findViewById(R.id.profileEmail);
+        EditText phoneField = (EditText) findViewById(R.id.profilePhone);
+        EditText nameField = (EditText) findViewById(R.id.profileName);
+        Button update = (Button) findViewById(R.id.profileUpdate);
+
+        score.setText(user.getScore());
         name.setText(user.getName());
+        emailField.setText(user.getEmail());
+        phoneField.setText(user.getPhone());
+        nameField.setText(user.getUsername());
+
+
 
         db.userDao().update(user);
     }
